b8cbd4c2ae213b1f82b4c608349562efd7dc4892
liemleba <liemle@mpire.vn>
2019-05-30 11:02:21 +0700

    update unit test

--- test_main.c
+++ test_main.c
@@ -4,6 +4,7 @@
 
 static unsigned int func_call[3];
 static volatile uint32_t tick;
+static int test_done = 0;
 
 WORKQ_DECLARE(main);
 WORKQ_ITEM_DECLARE(item1);
@@ -14,6 +15,7 @@ void fun1(struct workq_item *item)
 {
 	if (++func_call[0] >= 100) {
 		workq_cancel(&wq_main, item);
+		++test_done;
 	}
 }
 
@@ -21,6 +23,7 @@ void fun2(struct workq_item *item)
 {
 	if (++func_call[1] >= 300) {
 		workq_cancel(&wq_main, item);
+		++test_done;
 	}
 }
 
@@ -28,6 +31,7 @@ void fun3(struct workq_item *item)
 {
 	if (++func_call[2] >= 150) {
 		workq_cancel(&wq_main, item);
+		++test_done;
 	}
 }
 
@@ -43,7 +47,7 @@ int main(void)
 	workq_post_delayed(&wq_main, &wqi_item2, 1000);
 	workq_post_delayed(&wq_main, &wqi_item3, 2000);
 
-	while (1) {
+	while (test_done<3) {
 		if (workq_iterate(&wq_main, tick) == 0) {
 			//Sleep
 		}
@@ -53,5 +57,5 @@ int main(void)
 		}
 	}
 
-	return 0;
+	return 666;
 }
